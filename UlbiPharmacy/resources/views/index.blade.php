ec63cf915f1728e1f016a9199d698158bb920fdf
afifahnaufal <afifahrahmani466@gmail.com>
2024-01-21 18:20:35 +0700

    update

--- UlbiPharmacy/resources/views/index.blade.php
+++ UlbiPharmacy/resources/views/index.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.nav')
     
 @section('container')
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+    <!-- Full-width images with number and caption text -->
+    <div class="mySlides fade">
+      <div class="numbertext">1 / 2</div>
+      <img src="image/bg.jpg" style="width:100%">
+      <div class="text">Caption Text</div>
+    </div>
+  
+    <div class="mySlides fade">
+      <div class="numbertext">2 / 3</div>
+      <img src="image/p.jpg.jpg" style="width:100%">
+      <div class="text"></div>
+    </div>
+  
+    <!-- Next and previous buttons -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+  </div>
+  <br>
+  
+  <!-- The dots/circles -->
+  <div style="text-align:center">
+    <span class="dot" onclick="currentSlide(1)"></span>
+    <span class="dot" onclick="currentSlide(2)"></span>
+  </div>
+
 
     <button class="carousel-button" id="prev-button" onclick="prevSlide()">❮</button>
     <button class="carousel-button" id="next-button" onclick="nextSlide()">❯</button>
@@ -31,25 +59,52 @@
 
    
 
-<script>
-function scrollToDiv() {
-    const targetDiv = document.getElementById('About');
-    if (targetDiv) {
-        targetDiv.scrollIntoView({ behavior: 'smooth' });
-    }
-}
+    <script>
+        function scrollToDiv() {
+            const targetDiv = document.getElementById('About');
+            if (targetDiv) {
+                targetDiv.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+        
+        function scrollToDiv2() {
+            const targetDiv = document.getElementById('Contact');
+            if (targetDiv) {
+                targetDiv.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+        
+        let slideIndex = 1;
+        showSlides(slideIndex);
+        
+        // Next/previous controls
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+        
+        // Thumbnail image controls
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
+        
+        function showSlides(n) {
+          let i;
+          let slides = document.getElementsByClassName("mySlides");
+          let dots = document.getElementsByClassName("dot");
+          if (n > slides.length) {slideIndex = 1}
+          if (n < 1) {slideIndex = slides.length}
+          for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+          }
+          for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";
+          dots[slideIndex-1].className += " active";
+        }
+        
+        
+        </script>
 
-function scrollToDiv2() {
-    const targetDiv = document.getElementById('Contact');
-    if (targetDiv) {
-        targetDiv.scrollIntoView({ behavior: 'smooth' });
-    }
-}
 
-showSlide(currentIndex);
-
-</script>
-
-
-      
 @endsection('container')
